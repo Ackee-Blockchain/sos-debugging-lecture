6b076bfb633518f10ea4eed73035cab4d91d2de1
Ikrk <ikrk@centrum.cz>
2023-09-07 16:57:45 +0200

    AccountAlreadyInUse

--- tests/solana-errors.ts
+++ tests/solana-errors.ts
@@ -11,14 +11,9 @@ describe("solana-errors", () => {
   const user = Keypair.generate();
   const data = Keypair.generate();
 
-
-  // 1. Error message is not clear: Error: failed to send transaction: Transaction simulation failed: Error processing Instruction 0: custom program error: 0x1
-  // 2. You can try to use sendAndConfirmTransaction -> then the error is clear: Error: failed to send transaction: Transaction simulation failed: Attempt to debit an account but found no record of a prior credit.
-  // 3. Airdrop some lamports - do not forget to await the function call!
-
-  // before("prepare", async () => {
-  //   await airdrop(anchor.getProvider().connection, user.publicKey)
-  // })
+  before("prepare", async () => {
+    await airdrop(anchor.getProvider().connection, user.publicKey)
+  })
 
   it("Is initialized!", async () => {
 
@@ -31,12 +26,21 @@ describe("solana-errors", () => {
         systemProgram: SystemProgram.programId
       })
       .signers([user, data])
-      // .transaction()
       .rpc();
 
-    // await sendAndConfirmTransaction(connection, tx, [user, data], { commitment: "confirmed" });
-
     console.log("Your transaction signature", tx);
+
+    let t = await program.methods
+      .initialize()
+      .accounts({
+        user: user.publicKey,
+        data: data.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([user, data])
+      .rpc({ skipPreflight: true });
+
+    // await sendAndConfirmTransaction(connection, t.transaction(), [user, data], { skipPreflight: true });
   });
 });
 
